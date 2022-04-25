8d6969714ef9a9268ad7e6b8b8b3a241d4012f7d
UNIMAR\ti.estagiario <alissonruan567@gmail.com>
2022-04-25 12:28:00 -0300

    Update 2

--- config.php
+++ config.php
@@ -1,18 +1,14 @@
 <?php
 
 // coloque a barra / no final
-define('URL', 'https://appserver.unimar.br/intranet/');
+define('URL', 'http://localhost/estagio/MVC/');
 // **** OBS:> alterar também a url do preloader no main.css
-define('ABS_PATH', "/www/web/intranet/");
 
 define('LIBS', 'libs/');
+
 //conf. da conexao com o BD
-define('DB_TNS', '(DESCRIPTION =
-					   (ADDRESS = (PROTOCOL = TCP)(HOST = 172.16.6.12)(PORT = 1521))
-					   (CONNECT_DATA =
-						  (SERVER = DEDICATED)
-						  (SID = proj)
-						)
-					  )');
-define('DB_USER', 'AUTOATENDIMENTO');
-define('DB_PASS', 'AutoAten');
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost:3306');
+define('DB_NAME', 'teste'); // Name Database
+define('DB_USER', 'root');
+define('DB_PASS', '');
